e01dbbe3ce4c2b0c80b1c9b836c61c2ad512fa6a
Derek Foster <derekfoster94@gmail.com>
2021-03-24 10:16:54 -0700

    formatField

---
+++ utils.js
@@ -0,0 +1,26 @@
+/**
+ * [formatField is used to format JSON (camelCased) fields to human formatted fields, e.g. firstName --> First Name]
+ * @param  {String} field [JSON formatted field name]
+ * @return {String}       [Human formatted field name]
+ */
+//TODO: find a way to make this agnostic to length. will just work on ANY field
+// currently: only of the form fieldForm or just field
+function formatField(field) {
+  let regex = /^([a-z]+)([A-Z][a-z]+)?$/;
+
+  const groups = field.match(regex);
+
+  console.log(groups);
+
+  // groups[0] is the overall match
+  // groups[1, 2] are the capture groups
+  if (groups[2] === undefined) {
+    return uppercaseString(groups[1]);
+  } else {
+    return uppercaseString(groups[1]) + " " + uppercaseString(groups[2]);
+  }
+}
+
+function uppercaseString(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
